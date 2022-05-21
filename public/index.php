535df048024f596f3410236357df7f25a825ec6e
wm <wwwhitemode@gmail.com>
2022-05-21 23:53:38 +0500

    installing

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    $response->getBody()->write('Welcome to Slim!');
+    return $response;
+});
+$app->run();
