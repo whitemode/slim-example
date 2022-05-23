e081a6616ebbcc3099eb2e4a97e4d026ddbf1ede
wm <wwwhitemode@gmail.com>
2022-05-23 11:53:50 +0500

    add flash

--- public/index.php
+++ public/index.php
@@ -5,11 +5,20 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+session_start();
+
 $container = new Container();
+
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
-$app = AppFactory::createFromContainer($container);
+
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $categories = json_decode(file_get_contents("categories.json"), true);
@@ -23,19 +32,24 @@ $app->get('/', function ($request, $response) {
 
 $app->get('/categories', function ($request, $response) use ($categories) {
     $request = $request->getQueryParam('request');
+    $messages = $this->get('flash')->getMessages();
 
     if ($request !== '') {
         $desiredCategories = array_filter($categories, fn($category) => mb_strpos($category["name"], $request) !== false);
         $params = [
             "request" => $request,
-            "categories" => $desiredCategories
+            "categories" => $desiredCategories,
+            "flash" => $messages
         ];
     } else {
         $params = [
             "request" => $request,
-            "categories" => $categories
+            "categories" => $categories,
+            "flash" => $messages
         ];
     }
+
+    //print_r($params['flash']['success'][0]);
 
     return $this->get('renderer')->render($response, 'categories/categories.phtml', $params);
 });
@@ -58,6 +72,8 @@ $app->post('/categories', function ($request, $response) use ($categories) {
         
         file_put_contents("categories.json", json_encode($categories));
         
+        $this->get('flash')->addMessage('success', 'Категория добавлена успешно!');
+
         return $response->withRedirect('/categories', 302);
     }
 
@@ -66,6 +82,26 @@ $app->post('/categories', function ($request, $response) use ($categories) {
         'error' => 'Название категории не может быть пустым'
     ];
     return $this->get('renderer')->render($response, "categories/new.phtml", $params);
+});
+
+$app->get('/foo', function ($req, $res) {
+    // Добавление флеш-сообщения. Оно станет доступным на следующий HTTP-запрос.
+    // 'success' — тип флеш-сообщения. Используется при выводе для форматирования.
+    // Например можно ввести тип success и отражать его зелёным цветом (на Хекслете такого много)
+    $this->get('flash')->addMessage('success', 'This is a message');
+
+    return $res->withRedirect('/bar');
+});
+
+$app->get('/bar', function ($req, $res, $args) {
+    // Извлечение flash сообщений установленных на предыдущем запросе
+    $messages = $this->get('flash')->getMessages();
+    var_dump($_REQUEST);
+    echo "\n";
+    print_r($messages); // => ['success' => ['This is a message']]
+
+    $params = ['flash' => $messages];
+    $this->get('renderer')->render($res, 'bar.phtml', $params);
 });
 
 $app->run();
